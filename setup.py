005f9e2673b4f93903964c08ec6112d6e92a79ee
bioliyezhang <bioliyezhang@gmail.com>
2016-02-09 12:05:43 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tcga_survival',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='liye zhang',
+      author_email='bioliyezhang@gmail.com',
+      license='MIT',
+      packages=['tcga_survival'],
+      zip_safe=False)
